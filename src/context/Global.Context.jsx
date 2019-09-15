ea5ea051004867d8a3aab8135c58f2b2bc51d685
Lazaro <lquin008@gmail.com>
2019-09-15 12:25:20 -0400

    global context update

--- src/context/Global.Context.jsx
+++ src/context/Global.Context.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useReducer, useEffect, useCallback } from 'react';
-// import { auth, database } from '../firebase';
-// import { getCurrentUser } from '../helpers';
+import { auth, database } from '../firebase';
+import { getCurrentUser } from '../helpers';
 
 export const GlobalContext = createContext({});
 
@@ -13,23 +13,23 @@ export const GlobalContextProvider = ({ children }) => {
   const globalContextReducer = (state, payload) => ({ ...state, ...payload });
   const [globalState, setGlobalState] = useReducer(globalContextReducer, INITIAL_STATE);
   const createSession = async _ => {
-    // const { currentUser = {}, token } = (await getCurrentUser()) || {};
-    // if (!currentUser.uid) return;
-    // sessionStorage.setItem('Auth', token);
-    // database
-    //   .ref('/profiles')
-    //   .orderByChild('uid')
-    //   .equalTo(currentUser.uid)
-    //   .once('value', snapshot => {
-    //     const profile = snapshot.val();
-    //     const currentUserProfile = Object.values(profile)[0] || {};
-    //     setGlobalState({ currentUser, currentUserProfile });
-    //   });
+    const { currentUser = {}, token } = (await getCurrentUser()) || {};
+    if (!currentUser.uid) return;
+    sessionStorage.setItem('Auth', token);
+    database
+      .ref('/profiles')
+      .orderByChild('uid')
+      .equalTo(currentUser.uid)
+      .once('value', snapshot => {
+        const profile = snapshot.val();
+        const currentUserProfile = Object.values(profile)[0] || {};
+        setGlobalState({ currentUser, currentUserProfile });
+      });
   };
   const clearSession = async _ => {
-    // await auth.signOut();
-    // sessionStorage.removeItem('Auth');
-    // setGlobalState({ currentUser: {}, currentUserProfile: {} });
+    await auth.signOut();
+    sessionStorage.removeItem('Auth');
+    setGlobalState({ currentUser: {}, currentUserProfile: {} });
   }
   const mountEffect = useCallback(createSession, []);
   useEffect(
