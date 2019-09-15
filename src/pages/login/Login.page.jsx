b6538487b9089b1413b8fdcb0cb43c3f67fd11de
Lazaro <lquin008@gmail.com>
2019-09-15 13:11:48 -0400

    log in error updates

--- src/pages/login/Login.page.jsx
+++ src/pages/login/Login.page.jsx
@@ -1,12 +1,15 @@
 import React, { useContext, useState } from "react";
 import { auth } from "../../firebase";
-import { GlobalContext } from "../../context";
-import { SectionWrapper, Form } from "../../components";
+import { GlobalContext, ModalContext } from "../../context";
+import { SectionWrapper, Form, Loader } from "../../components";
 import { schema } from "./Login.schema";
 
 export default ({ history }) => {
   const { createSession } = useContext(GlobalContext);
+  const { setModal } = useContext(ModalContext);
+  const [loading, setLoading] = useState(false);
   const handleLogin = async formData => {
+    setLoading(true);
     try {
       const { email, password } = formData;
       const { user } = await auth.signInWithEmailAndPassword(email, password);
@@ -15,11 +18,21 @@ export default ({ history }) => {
       history.push("/my-profile");
     } catch (error) {
       console.log("error loging in", error);
+      setModal({
+        isOpen: true,
+        content: "Could not login with these credentials, please try again."
+      });
+    } finally {
+      setLoading(false);
     }
   };
   return (
     <SectionWrapper columnDefs="col-md-6 col-md-offset-3">
-      <Form schema={schema} handleSubmit={handleLogin} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <Form schema={schema} handleSubmit={handleLogin} />
+      )}
     </SectionWrapper>
   );
 };
