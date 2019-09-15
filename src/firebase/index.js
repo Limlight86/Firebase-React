2132044660ef0f9b7df8e512d3a169592888a184
Lazaro <lquin008@gmail.com>
2019-09-15 15:49:55 -0400

    changed firebase server

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from "firebase";
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "fir-tutorial-a8292.firebaseapp.com",
-  databaseURL: "https://fir-tutorial-a8292.firebaseio.com",
-  projectId: "fir-tutorial-a8292",
+  authDomain: "react-firebase-4625c.firebaseapp.com",
+  databaseURL: "https://react-firebase-4625c.firebaseio.com",
+  projectId: "react-firebase-4625c",
   storageBucket: "",
-  messagingSenderId: "67469922153",
+  messagingSenderId: "840202695025",
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
