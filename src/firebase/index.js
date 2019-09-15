18161a2bd1f5dfb2ca5ad0efab3f43e7713cbb27
Lazaro <lquin008@gmail.com>
2019-09-15 11:57:51 -0400

    update firebase index

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,11 +1,16 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'fir-tutorial-a8292.firebaseapp.com',
-  databaseURL: 'https://fir-tutorial-a8292.firebaseio.com',
-  projectId: 'fir-tutorial-a8292',
-  storageBucket: '',
-  messagingSenderId: '67469922153',
+  authDomain: "fir-tutorial-a8292.firebaseapp.com",
+  databaseURL: "https://fir-tutorial-a8292.firebaseio.com",
+  projectId: "fir-tutorial-a8292",
+  storageBucket: "",
+  messagingSenderId: "67469922153",
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
+const app = firebase.initializeApp(config);
+
+export const database = app.database();
+export const auth = app.auth();
