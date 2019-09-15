7af733d02429767a1f5e78e213f388e5307a25e2
Lazaro <lquin008@gmail.com>
2019-09-15 12:53:56 -0400

    login and logout works

--- src/components/access_button/AccessButton.component.jsx
+++ src/components/access_button/AccessButton.component.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react';
-import { Link, NavLink, withRouter } from 'react-router-dom';
-// import { GlobalContext } from '../../context';
-import { isLoggedIn } from '../../helpers';
+import React, { useContext } from "react";
+import { Link, NavLink, withRouter } from "react-router-dom";
+import { GlobalContext } from "../../context";
+import { isLoggedIn } from "../../helpers";
 
 const AccessButton = _ => {
-  // const { clearSession } = useContext(GlobalContext);
+  const { clearSession } = useContext(GlobalContext);
   return isLoggedIn() ? (
-    <Link to="/" onClick={_ => console.log('handling logout')}>
+    <Link to="/" onClick={clearSession}>
       Logout
     </Link>
   ) : (
--- src/pages/login/Login.page.jsx
+++ src/pages/login/Login.page.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { SectionWrapper, Form } from '../../components';
-import { schema } from './Login.schema';
+import React, { useContext, useState } from "react";
+import { auth } from "../../firebase";
+import { GlobalContext } from "../../context";
+import { SectionWrapper, Form } from "../../components";
+import { schema } from "./Login.schema";
 
-export default _ =>  (
+export default ({ history }) => {
+  const { createSession } = useContext(GlobalContext);
+  const handleLogin = async formData => {
+    try {
+      const { email, password } = formData;
+      const { user } = await auth.signInWithEmailAndPassword(email, password);
+      if (!user) return;
+      await createSession();
+      history.push("/my-profile");
+    } catch (error) {
+      console.log("error loging in", error);
+    }
+  };
+  return (
     <SectionWrapper columnDefs="col-md-6 col-md-offset-3">
-      <Form schema={schema} handleSubmit={_ => console.log('submiting login form')} />
+      <Form schema={schema} handleSubmit={handleLogin} />
     </SectionWrapper>
   );
+};
