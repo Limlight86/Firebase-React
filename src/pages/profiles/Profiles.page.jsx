a4f5bf48a13041e4573efb9c0e714bde3e5e12f9
Lazaro <lquin008@gmail.com>
2019-09-15 14:56:47 -0400

    view profiles populated

--- src/pages/profiles/Profiles.page.jsx
+++ src/pages/profiles/Profiles.page.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
-import { SectionWrapper } from '../../components';
+import React, { useState, useEffect } from "react";
+import { withRouter } from 'react-router-dom'
+import { database } from "../../firebase";
+import { SectionWrapper, Profile } from "../../components";
 
-export default _ => (
-  <SectionWrapper>
-    All the profiles
-  </SectionWrapper>
-);
+const Profiles = ({ history }) => {
+  const [profiles, setProfiles] = useState([]);
+  const [selectedProfileId, setSelectedProfileId] = useState(String());
+
+  const subscribeToProfiles = () => {
+    database.ref("/profiles").on("value", snapshot => {
+      const profiles = snapshot.val();
+      const parsedProfiles = Object.values(profiles);
+      setProfiles(parsedProfiles);
+    });
+  };
+
+  const handleProfileClick = (id) => () => setSelectedProfileId(id)
+  const handleViewProfile = () => history.push(`/selected-profile/${selectedProfileId}`)
+
+  useEffect(() => {
+    subscribeToProfiles();
+  }, []);
+
+  return (
+    <SectionWrapper>
+      <h2>Select a Proifle to View</h2>
+      <ul>
+        {profiles.map(profile => (
+          <Profile
+            key={profile.id}
+            {...profile}
+            handleProfileClick={handleProfileClick}
+            handleViewProfile={handleViewProfile}
+            selectedProfileId={selectedProfileId}
+          />
+        ))}
+      </ul>
+    </SectionWrapper>
+  );
+};
+
+export default withRouter(Profiles)
