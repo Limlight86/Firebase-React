2a5c0724a2eb8a2dd767ceb0ec5c370a743ffcfc
Lazaro <lquin008@gmail.com>
2019-09-15 15:13:14 -0400

    selected profile works

--- src/pages/selected_profile/SelectedProfile.page.jsx
+++ src/pages/selected_profile/SelectedProfile.page.jsx
@@ -1,4 +1,34 @@
-import React from 'react';
-import { SectionWrapper } from '../../components';
+import React, { useState, useEffect, useContext } from "react";
+import { withRouter } from "react-router-dom";
+import { database } from "../../firebase";
+import { GlobalContext } from "../../context";
+import { SectionWrapper, ProfileContent, Comments } from "../../components";
 
-export default _ => <SectionWrapper>Selected Profile Page</SectionWrapper>;
+const SelectedProfile = ({ match }) => {
+  const [selectedProfile, setSeletectedProfile] = useState(null);
+  const { profileId } = match.params || {};
+  const fetchSelectedProfile = () => {
+    database
+    .ref('/profiles')
+    .orderByChild('uid')
+    .equalTo(profileId)
+    .once('value', snapshot => {
+      const selectedProfile = snapshot.val() || {}
+      const parsedSelectedProfile = Object.values(selectedProfile)[0]
+      setSeletectedProfile(parsedSelectedProfile)
+    })
+  }
+
+  useEffect(()=>{
+    fetchSelectedProfile()
+  }, [])
+
+  return(
+    <SectionWrapper>
+      {selectedProfile && <ProfileContent {...selectedProfile} /> }
+      <Comments />
+    </SectionWrapper>
+  ) 
+};
+
+export default withRouter(SelectedProfile);
